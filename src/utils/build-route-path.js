7ea2f126e1376d10b2da76d394bead84e91b474a
krasny <lookjota@gmail.com>
2023-04-22 09:18:08 -0300

    fundamentos nodejs

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,10 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-]+)')
+
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
+
+  pathRegex.test 
+
+  return pathRegex
+}
